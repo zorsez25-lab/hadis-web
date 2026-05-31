2947bef0e73e6c8efa6ae0aeccad22b4733fc0f3
Sezer <zorsez25@gmail.com>
2026-05-31 18:10:47 +0300

    fix: RaviListesiWrapper filtre prop

--- app/raviler/RaviListesiWrapper.tsx
+++ app/raviler/RaviListesiWrapper.tsx
@@ -1,5 +1,6 @@
 import RaviListesi from './RaviListesi';
+import { TabakaFiltre } from '@/lib/ravi-utils';
 
-export default async function RaviListesiWrapper({ sorgu }: { sorgu: string }) {
-  return <RaviListesi sorgu={sorgu} />;
+export default async function RaviListesiWrapper({ sorgu, filtre }: { sorgu: string; filtre: TabakaFiltre }) {
+  return <RaviListesi sorgu={sorgu} filtre={filtre} />;
 }
